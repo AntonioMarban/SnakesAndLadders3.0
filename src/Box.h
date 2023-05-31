3877fd30c07f066b28b38fd37c2c23d346d65872
CarolRendonG <a01425341@tec.mx>
2023-05-30 18:27:21 -0600

    clase Box

--- src/Box.h
+++ src/Box.h
@@ -1,2 +1,33 @@
 #include <iostream>
 using namespace std;
+#pragma once
+
+class Box {
+  char boxType;
+  int boxNum;
+public:
+    Box() {};
+    Box(const char &boxType,const int &boxNum) : boxType(boxType), boxNum(boxNum){};
+    ~Box() {};
+    // Getters
+    char getboxType();
+    int getboxNum();
+    // Setters
+    void setboxType(char);
+    void setboxNum(int);
+};
+
+char Box::getboxType() {
+    return boxType;
+}
+
+int Box::getboxNum() {
+    return boxNum;
+}
+void Box::setboxType(char _boxType) {
+    boxType = _boxType;
+}
+
+void Box::setboxNum(int _boxNum) {
+    boxNum = _boxNum;
+}
