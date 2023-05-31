2dca8f6ae883ae4482a5c9089ec32a7664e803a7
AntonioMarban <393tony393@gmail.com>
2023-05-30 18:45:35 -0600

    idk

--- src/Board.h
+++ src/Board.h
@@ -1,11 +1,52 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include 
+#include "Box.h"
 using namespace std;
 
 class Board {
-    vector<Square>
+    int numberOfBoxes;
+    vector<Box*> boxes; // vector con apuntadores tipo box
+    
+    public:
+        Board() = default; // constructor por omisión
+        Board(const int&, const vector<Box*>); // constructor por argumentos
+        ~Board();
+
+        int getNumberOfBoxes();
+        vector<Box*> getBoxes();
+
+        void setNumberOfBoxes(int);
+        void setBoxes(vector<Box*>);
+
+        void printBoard();
+        
+};
+
+Board::Board(const int& numberOfBoxes, const vector<Box*> boxes) {
+
+    this->numberOfBoxes = numberOfBoxes;
+    this->boxes = boxes;
 }
+
+Board::~Board() {}
+
+int Board::getNumberOfBoxes() {
+    return numberOfBoxes;
+}
+
+vector<Box*> Board::getBoxes() {
+    return boxes;
+}
+
+void Board::setNumberOfBoxes(int numberOfBoxes) {
+    this->numberOfBoxes = numberOfBoxes;
+}
+
+void Board::setBoxes(vector<Box*> boxes) {
+    this->boxes = boxes;
+}
+
+
 
 
