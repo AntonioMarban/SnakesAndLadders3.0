b60d6cbe9432042a0bb312f7c95e8b16c0b49801
CarolRendonG <a01425341@tec.mx>
2023-05-30 21:27:27 -0600

    clase

---
+++ src/Player.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include "Person.h"
+using namespace std;
+
+class Player : public Person // la clase Player hereda con ambito publico de la clase Person
+{
+    int playerNum;
+    int position;
+public:
+    Player() {};
+    Player(const string &name) : Person(name){}
+    Player(const int &playerNum, const int &position) : playerNum(playerNum), position(position) {}; 
+    ~Player() {};
+    // Getters
+    int getPlayerNum();
+    int getPosition();
+    // Setters
+    void setPlayerNum(int);
+    void setPosition(int);
+};
+
+int Player::getPlayerNum() {
+    return playerNum;
+}
+
+void Player::setPlayerNum(int _playerNum) {
+    playerNum = _playerNum;
+}
+
+int Player::getPosition() {
+    return position;
+}
+
+void Player::setPosition(int _position) {
+    position = _position;
+}
